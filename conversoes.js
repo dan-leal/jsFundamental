bda3bb4a1bc410dec8153463e4e6e9eeaaa7f7e7
Daniel Leal <daniel_leal25@gmail.com>
2021-12-10 11:40:47 -0400

    comitando

---
+++ conversoes.js
@@ -0,0 +1,11 @@
+//tipo de dados
+// boleano
+
+// conversão implícita
+
+const num = 456
+const numString = '456'
+
+console.log(num + Number(numString))
+
+// conversão explícita
---
+++ type-number.js
@@ -0,0 +1,24 @@
+// boleano
+const usuarioLogado = true
+const contapaga = false
+
+// truthy ou falsy
+
+// 0 => false
+// 1 => true
+
+// console.log(1 == true)
+
+// null e undefined
+
+let minhaVar
+let varNull = null
+
+console.log(minhaVar)
+console.log(varNull)
+
+let numero = 3
+let texto = 'Alura'
+
+console.log(typeof minhaVar)
+console.log(typeof varNull)
